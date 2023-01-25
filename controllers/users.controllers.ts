a81fbca6cb96b596fb4f98f5f32b440e3c9c1f6d
gurungpratik <pratikgurung365@gmail.com>
2023-01-25 18:09:13 +0000

    completed first end point

--- controllers/users.controllers.ts
+++ controllers/users.controllers.ts
@@ -5,7 +5,7 @@ exports.getUserById = (req, res, next) =>{
     
     selectUserById(req.params.user_id).then((user)=>{
         console.log(user, "user in controller");
-        res.status(200).send(user)
+        res.status(200).send({ user })
     }).catch((err)=>{
         console.log(err, "err in controller");
         // next()
--- models/users.models.ts
+++ models/users.models.ts
@@ -3,9 +3,12 @@ import { db } from "../db/connection";
 import { loginRegister } from "../db/seeds/schemas/loginRegisterSchema";
 
 exports.selectUserById = async (user_id) => {
-    console.log(await db.model("user",loginRegister).find({}), "<<<<<< test");
-   await db.model("user",loginRegister).find({}).then((data)=>{
-        console.log(data, "data in model");
-        return data
-    })
-}
+  console.log(await db.model("user", loginRegister).find({}), "<<<<<< test");
+  const dbModel = await db.model("user", loginRegister);
+
+  console.log(dbModel, "this is the dbmodel")
+
+  const user = await dbModel.aggregate().unwind({ path: `$users` }).match({ "users.user_id": `${user_id}`})
+
+  return user[0].users;
+};
