736f83d923633cabd1090bcb484a5d777a0417be
gurungpratik <pratikgurung365@gmail.com>
2023-01-27 19:59:36 +0000

    updated patchhouseholdusers controller

--- controllers/households.controllers/patchHouseholdUsers.ts
+++ controllers/households.controllers/patchHouseholdUsers.ts
@@ -1,5 +1,9 @@
-import { updateHouseholdUsers } from "../../models/households.models/updateHouseholdUsers";
+// import { updateHouseholdUsers } from "../../models/households.models/updateHouseholdUsers";
+
+const { updateHouseholdUsers } = require("../../models/households.models/updateHouseholdUsers")
 
 export function patchHouseholdUsers (req, res, next) {
-    updateHouseholdUsers(req)
+    updateHouseholdUsers(req.body)
+    
 }
+
