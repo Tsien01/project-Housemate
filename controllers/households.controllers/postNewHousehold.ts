6bb1570c628fe4f29198e526b29223560796e82e
333Abe <adrianbennett333@gmail.com>
2023-01-28 11:00:36 +0000

    initial commit

--- controllers/households.controllers/postNewHousehold.ts
+++ controllers/households.controllers/postNewHousehold.ts
@@ -3,3 +3,22 @@ import { insertNewHousehold } from "../../models/households.models/insertNewHous
 export function postNewHousehold (req, res, next) {
     insertNewHousehold(req)
 }
+
+
+
+// post user to be added to new household
+
+// checks to see if user is already part of another household
+// if not part of existing household, create new household with one user with admin permissions
+// Request body accepts:
+
+// an object in the form:
+// {
+// user_email: <user's email>,
+// name: <user's name>
+// household_name: <name of household trying to join>,
+// household_password: <password of household>
+// }
+
+// permissions set to member
+// currScore set to 0
