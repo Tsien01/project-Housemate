933eca0651e7fc39bf9289177692cd872f7d0897
333Abe <adrianbennett333@gmail.com>
2023-01-29 12:11:24 +0000

    task 5 complete

---
+++ utils/utils.ts
@@ -0,0 +1,11 @@
+function validateEmail(input) {
+    const ef =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    if (input.match(ef)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  module.exports = { validateEmail };
