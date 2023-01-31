16f66cccaca6e864bc2a0d46b7c200cf76990cf2
Tsien01 <tsien.jack@gmail.com>
2023-01-31 17:03:23 +0000

    fixed a bug with deleteTaskByTaskId

--- models/households.models/deleteTaskByTaskId.ts
+++ models/households.models/deleteTaskByTaskId.ts
@@ -8,6 +8,13 @@ export async function deleteTaskByTaskId (taskBody) {
     const householdModel = connection.model("household", householdObjectSchema)
 
     let household = await householdModel.find({ "users.email": taskBody.email })
+    
+    if (household.length === 0) {
+        return Promise.reject({
+            message: "404 Not found",
+            status: 404,
+        })
+    }
 
     household[0].tasks = household[0].tasks.filter((task) => {
         if (task.email === taskBody.email && task.title === taskBody.title && task.created_at===taskBody.created_at && task.task_value===taskBody.task_value) {
